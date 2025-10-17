b7a2162b6446985888ce44afd888cf3c1b0f3eda
lemaria0 <leticiamaria200518@gmail.com>
2025-10-16 23:00:06 -0300

    primeira questão - alternativa c

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package main;
+import races.RacerThread;
 
-/**
- *
- * @author conta
- */
 public class Main {
-    
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            RacerThread racer = new RacerThread(i);
+            
+            try {
+                racer.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            
+            racer.start();
+        }
+    }
 }
